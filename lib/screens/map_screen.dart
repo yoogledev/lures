752f1f35687c502e62e5638beda0f542f7f0ddd9
yoogle <dmltjs851@gmail.com>
2025-10-13 03:44:39 +0900

    refactor: update application ID and namespace to dev.yoogle.lures; enhance map screen with Naver Map integration and initial camera setup

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,14 +1,29 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Map Screen'),
+    const seoulCityHall = NLatLng(37.5666, 126.979);
+    final safeAreaPadding = MediaQuery.paddingOf(context);
+    return Scaffold(
+      body: NaverMap(
+        options: NaverMapViewOptions(
+          contentPadding: safeAreaPadding,
+          initialCameraPosition: const NCameraPosition(target: seoulCityHall, zoom: 14),
+        ),
+        onMapReady: (controller) {
+          final marker = NMarker(
+            id: "city_hall",
+            position: seoulCityHall,
+            caption: const NOverlayCaption(text: "Seoul City Hall"),
+          );
+          controller.addOverlay(marker);
+          print("naver map is ready!");
+        },
       ),
     );
   }
