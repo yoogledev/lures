9d34ce4c50a3cd98e1f8126d6a298e179bab3d42
yoogle <dmltjs851@gmail.com>
2025-10-22 22:00:35 +0900

    feat: integrate geolocation functionality with Naver Map; add necessary permissions and update dependencies

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,9 +1,16 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:geolocator/geolocator.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  NaverMapController? _controller;
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +21,10 @@ class MapScreen extends StatelessWidget {
         options: NaverMapViewOptions(
           contentPadding: safeAreaPadding,
           initialCameraPosition: const NCameraPosition(target: seoulCityHall, zoom: 14),
+          locationButtonEnable: true,
         ),
         onMapReady: (controller) {
+          _controller = controller;
           final marker = NMarker(
             id: "city_hall",
             position: seoulCityHall,
@@ -25,6 +34,42 @@ class MapScreen extends StatelessWidget {
           print("naver map is ready!");
         },
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _goToCurrentLocation,
+        child: const Icon(Icons.my_location),
+      ),
     );
+  }
+
+  Future<void> _goToCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    final position = await Geolocator.getCurrentPosition();
+    final nLatLng = NLatLng(position.latitude, position.longitude);
+
+    final cameraUpdate = NCameraUpdate.withParams(
+      target: nLatLng,
+      zoom: 15,
+    );
+    _controller?.updateCamera(cameraUpdate);
   }
 }
